2f7c05642cdf8006b544fa1dc481641ff54b4c9d
Vettvangur\ingimar <ingimar@vettvangur.is>
2023-07-31 14:12:46 +0000

    remove ps release build script and add some files to sln

--- release-build.ps1
+++
@@ -1,6 +0,0 @@
-# Don't use - Azure DevOps handles publishing
-dotnet pack .\Ekom\ -c Release --include-symbols -o .
-dotnet pack .\Ekom.Web\ -c Release --include-symbols -o .
-# $pkg = gci *.nupkg 
-# nuget push $pkg -Source https://www.nuget.org/api/v2/package -NonInteractive
-pause
