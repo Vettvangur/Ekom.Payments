57ec0d08bd6c2236533ce72f82a135a1be36aa40
Vettvangur\thorvardurb <thorvardurb@vettvangur.is>
2026-08-26 17:18:25 +0000

    change default settings

--- PaymentProviders/Ekom.Payments.SiminnPay/Model/SiminnPaySettings.cs
+++ PaymentProviders/Ekom.Payments.SiminnPay/Model/SiminnPaySettings.cs
@@ -10,7 +10,7 @@ public class SiminnPaySettings : PaymentSettingsBase<SiminnPaySettings>
     public string? PaymentFormUrl { get; set; }
     public string? CustomerInformationSharedSecret { get; set; }
     public bool RestrictToLoan { get; set; }
-    public string PaymentType { get; set; } = "card";
+    public string PaymentType { get; set; } = "loan";
     public int TimeToLive { get; set; } = 60;
     public string ReferenceId { get; set; }
     public string Currency { get; set; } = "ISK";
